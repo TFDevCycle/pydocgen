a2c22389b6460b4e16e0a137d16f3405b13357e8
DevCycle <72951068+TFDevCycle@users.noreply.github.com>
2023-06-01 19:12:17 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='mydocgenerator',
     version='1.0',
     packages=['mydocgenerator'],
+    long_description="""# Markdown supported!\n\n* Cheer\n* Celebrate\n""",
     entry_points={
         'console_scripts': [
             'mydocgenerator = mydocgenerator.mydocgenerator:main'
